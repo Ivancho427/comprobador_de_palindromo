5d23348cc1f50894b509fb50a1be4036e94881b2
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-08-17 21:49:32 -0500

    Ejercicio Palidromo

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function palindrome(str) {    
+  let comparar = str.match(/[a-z0-9]/ig).join("").toLowerCase();
+  str = str.match(/[a-z0-9]/ig).reverse().join('').toLowerCase();
+  console.log(comparar === str);
+  return comparar === str;
+
+  }  
+  
+palindrome();
+
+
+
+
+
+// palindrome("eye") debería devolver un booleano.
+// palindrome("eye") debería devolver verdadero.
+// palindrome("_eye") debería devolver verdadero.
+// palindrome("race car") debería devolver verdadero.
+// palindrome("not a palindrome") debe devolver falso.
+// palindrome("A man, a plan, a canal. Panama") debería devolver verdadero.
+// palindrome("never odd or even") debería devolver verdadero.
+// palindrome("nope") debe devolver falso.
+// palindrome("almostomla") debe devolver falso.
+// palindrome("My age is 0, 0 si ega ym.") debería devolver verdadero.
+// palindrome("1 eye for of 1 eye.") debe devolver falso.
+// palindrome("0_0 (: /-\ :) 0-0") debería devolver verdadero.
+// palindrome("five|\_/|four") debe devolver falso.
